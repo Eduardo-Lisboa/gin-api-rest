f8a2961ad014d9168019934951c683197b870288
Eduardo Henrique Lisboa Alves <henriqueeduardo63@gmail.com>
2022-12-05 08:51:06 -0300

    update

--- config/config.go
+++ config/config.go
@@ -10,3 +10,11 @@ const (
 const (
 	StringConectDatabase = "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
 )
+
+//config routes
+const (
+	//api name
+	ApiName = "api/v1"
+	//products
+	Products = "products"
+)
--- server/routes/routes.go
+++ server/routes/routes.go
@@ -1,15 +1,16 @@
 package routes
 
 import (
+	"github.com/Eduardo-Lisboa/api-go-gin/config"
 	"github.com/Eduardo-Lisboa/api-go-gin/controllers"
 	"github.com/gin-gonic/gin"
 )
 
 func ConfigureRoutes(router *gin.Engine) *gin.Engine {
 
-	main := router.Group("api/v1")
+	main := router.Group(config.ApiName)
 	{
-		products := main.Group("products")
+		products := main.Group(config.Products)
 		{
 			products.GET("/", controllers.Products)
 			products.GET("/:id", controllers.SearchForProduct)
